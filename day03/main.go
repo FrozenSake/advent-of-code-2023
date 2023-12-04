0d8c19b9795d6f2a198b7918f57593107f5f7746
FrozenSake <frozensake@gmail.com>
2023-12-04 02:56:42 -0500

    Complete Part 2 of Day 3

--- day03/main.go
+++ day03/main.go
@@ -20,14 +20,15 @@ func loadInput(sugar *zap.SugaredLogger) string {
 	return string(content)
 }
 
-func parseLine(line string, y int, sugar *zap.SugaredLogger) int {
+func parseLine(line string, y int, sugar *zap.SugaredLogger) (int, int) {
 	sum := 0
 
 	numbers := "0123456789"
 	offset := 1
+	numberOne, numberTwo := 0, 0
 
-	if len(line) <= 2 {
-		return 0
+	if len(line) <= 0 {
+		return 0, 0
 	}
 
 	num := ""
@@ -57,6 +58,7 @@ func parseLine(line string, y int, sugar *zap.SugaredLogger) int {
 				sugar.Errorf("Could not convert %s to number: %v", num, err)
 			}
 			sum += number
+			numberOne = number
 			num = ""
 		}
 	}
@@ -84,36 +86,64 @@ func parseLine(line string, y int, sugar *zap.SugaredLogger) int {
 			sugar.Errorf("Could not convert %s to number: %v", num, err)
 		}
 		sum += number
+		numberTwo = number
 		num = ""
 	}
 
 	sugar.Debugf("Called on line: %v\nSymbol pos: %v\nSum for symbol: %v\n", line, y, sum)
 
-	return sum
+	return sum, numberOne * numberTwo
 }
 
-func day3(input string, sugar *zap.SugaredLogger) int {
+func day3(input string, sugar *zap.SugaredLogger) (int, int) {
 	lines := strings.Split(input, "\n")
 	sum := 0
+	gearSum := 0
 
 	symbols := "#%&*+-/=@$"
 
 	var lastLine string
 
 	// This logic falls apart if there's ever a number adjacent to two symbols because it won't be excluded. Would need list of points and to validate the point wasn't already recorded to fix.
+	// Part 2 logic fails if there are ever > 2 numbers next to a gear, but there are not.
 	for i := 0; i < len(lines); i++ {
 
 		// Iterate the string looking for symbols.
 		for j, character := range lines[i] {
 			char := string(character)
 			if strings.Contains(symbols, char) {
-				sum += parseLine(lastLine, j, sugar)
+				numOne, gearRatio := parseLine(lastLine, j, sugar)
+				gearSum += gearRatio
+				sum += numOne
 				sugar.Debugf("Sum: %v\n", sum)
-				sum += parseLine(lines[i], j, sugar)
+				numTwo, gearRatio := parseLine(lines[i], j, sugar)
+				gearSum += gearRatio
+				sum += numTwo
 				sugar.Debugf("Sum: %v\n", sum)
-				if i+1 < len(lines) {
-					sum += parseLine(lines[i+1], j, sugar)
+				numThree := 0
+				if i+1 <= len(lines) {
+					numThree, gearRatio = parseLine(lines[i+1], j, sugar)
+					gearSum += gearRatio
+					sum += numThree
 					sugar.Debugf("Sum: %v\n", sum)
+				}
+				// Brute Forcing Part 2
+				if strings.Contains("*", char) {
+					if (numOne != 0 && numTwo != 0 && numThree == 0) || (numOne != 0 && numTwo == 0 && numThree != 0) || (numOne == 0 && numTwo != 0 && numThree != 0) {
+
+						if numOne == 0 {
+							numOne = 1
+						}
+						if numTwo == 0 {
+							numTwo = 1
+						}
+						if numThree == 0 {
+							numThree = 1
+						}
+						ratio := (numOne * numTwo * numThree)
+						gearSum = gearSum + ratio
+						sugar.Infof("one: %v \ntwo: %v \n three: %v \nratio: %v \ngearsum: %v \n", numOne, numTwo, numThree, ratio, gearSum)
+					}
 				}
 				sugar.Debugf("------")
 			}
@@ -121,7 +151,7 @@ func day3(input string, sugar *zap.SugaredLogger) int {
 		lastLine = lines[i]
 	}
 
-	return sum
+	return sum, gearSum
 }
 
 func main() {
@@ -130,8 +160,8 @@ func main() {
 	sugar := logger.Sugar()
 
 	input := loadInput(sugar)
-	sugar.Infof("%v", input)
+	sugar.Debugf("%v", input)
 
-	output := day3(input, sugar)
-	sugar.Infof("%v", output)
+	output, gearOutput := day3(input, sugar)
+	sugar.Infof("sum: %v\ngearSum: %v", output, gearOutput)
 }
