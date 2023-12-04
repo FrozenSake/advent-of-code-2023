d7a4a042ce5b518dfb36b383d9075b8adb4f393a
FrozenSake <frozensake@gmail.com>
2023-12-04 02:13:21 -0500

    Day 3

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
 )
@@ -18,11 +18,21 @@ func loadInput(sugar *zap.SugaredLogger) string {
 	return string(content)
 }
 
+func day(input string, sugar *zap.SugaredLogger) int {
+	lines := strings.Split(input, "\n")
+	sum := 0
+
+	return sum
+}
+
 func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 	sugar := logger.Sugar()
 
 	input := loadInput(sugar)
-	fmt.Println(input)
+	sugar.Infof("%v", input)
+
+	output := day(input, sugar)
+	sugar.Infof("%v", output)
 }
